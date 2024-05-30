74226e72ecf8ff21f75b6bf1210787b31d6298ae
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-05-30 14:45:00 +0200

    feat(CoursesList.js): add variable and conditional branching

--- Build/CourseList.js
+++ Build/CourseList.js
@@ -1,8 +1,12 @@
-function addlist(){
+function addlist() {
+  let list = document.getElementById("courseList");
+  let course = document.createElement("li");
+  let text = document.createTextNode(document.getElementById("list").value);
 
-    let list = document.getElementById("courseList");
-    let course = document.createElement("li");
-    let text = document.createTextNode(document.getElementById("list").value);
+    if (document.getElementById("list").value == "") {
+        return false;
+    }
     course.appendChild(text);
     list.appendChild(course);
+
 }
