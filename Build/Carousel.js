92947f15f3350eb12778c7c8e1a68f66e2be184d
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-05-30 12:42:24 +0200

    feat(Carousel.js): add "previous" function for carousel

--- Build/Carousel.js
+++ Build/Carousel.js
@@ -1,11 +1,9 @@
-let count = 0;
-function carousel() {
 
-    let red = document.querySelector(".red");
-    let green = document.querySelector(".green");
-    let blue = document.querySelector(".blue");
-    let purple = document.querySelector(".purple");
-    
+
+
+function next() {
+
+
 
     
         if (count == 0) {
@@ -24,11 +22,53 @@ function carousel() {
             return count = 3;
             
         } else if (count == 3) {
-            green.setAttribute("class", "slide purple inactive");
-            blue.setAttribute("class", "slide red active");  
+            purple.setAttribute("class", "slide purple inactive");
+            red.setAttribute("class", "slide red active");  
             return count = 0;
            
         }
         
-        document.getElementById('next').addEventListener('click',carousel());
+
 }
+
+function previous() {
+
+
+    
+
+    
+        if (count == 0) {
+            red.setAttribute("class", "slide red inactive");
+            purple.setAttribute("class", "slide purple active");                 
+            return count = 1;
+            
+        } else if (count == 1) {
+            purple.setAttribute("class", "slide purple inactive");
+            blue.setAttribute("class", "slide blue active");  
+            return count = 2;
+            
+        } else if (count == 2) {
+            blue.setAttribute("class", "slide blue inactive");
+            green.setAttribute("class", "slide green active");     
+            return count = 3;
+            
+        } else if (count == 3) {
+            green.setAttribute("class", "slide green inactive");
+            red.setAttribute("class", "slide red active");  
+            return count = 0;
+           
+        }
+        
+
+}
+
+
+let red = document.querySelector(".red");
+let green = document.querySelector(".green");
+let blue = document.querySelector(".blue");
+let purple = document.querySelector(".purple");
+let count = 0;
+
+
+document.getElementById('next').addEventListener('click',next());
+document.getElementById('next').addEventListener('click',next());
