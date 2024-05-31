421968aefa8e7f77de0281233242858ffa4fa679
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-05-31 09:24:06 +0200

    fix(Carousel.js): fix function issue

--- Build/Carousel.js
+++ Build/Carousel.js
@@ -1,67 +1,42 @@
-
-
-
 function next() {
-
-
-
-    
-        if (count == 0) {
-            red.setAttribute("class", "slide red inactive");
-            green.setAttribute("class", "slide green active");                   
-            return count = 1;
-            
-        } else if (count == 1) {
-            green.setAttribute("class", "slide green inactive");
-            blue.setAttribute("class", "slide blue active");  
-            return count = 2;
-            
-        } else if (count == 2) {
-            blue.setAttribute("class", "slide blue inactive");
-            purple.setAttribute("class", "slide purple active");  
-            return count = 3;
-            
-        } else if (count == 3) {
-            purple.setAttribute("class", "slide purple inactive");
-            red.setAttribute("class", "slide red active");  
-            return count = 0;
-           
-        }
-        
-
+  if (count == 0) {
+    red.setAttribute("class", "slide red inactive");
+    green.setAttribute("class", "slide green active");
+    return (count = 1);
+  } else if (count == 1) {
+    green.setAttribute("class", "slide green inactive");
+    blue.setAttribute("class", "slide blue active");
+    return (count = 2);
+  } else if (count == 2) {
+    blue.setAttribute("class", "slide blue inactive");
+    purple.setAttribute("class", "slide purple active");
+    return (count = 3);
+  } else if (count == 3) {
+    purple.setAttribute("class", "slide purple inactive");
+    red.setAttribute("class", "slide red active");
+    return (count = 0);
+  }
 }
 
 function previous() {
-
-
-    
-
-    
-        if (count == 0) {
-            red.setAttribute("class", "slide red inactive");
-            purple.setAttribute("class", "slide purple active");                 
-            return count = 1;
-            
-        } else if (count == 1) {
-            purple.setAttribute("class", "slide purple inactive");
-            blue.setAttribute("class", "slide blue active");  
-            return count = 2;
-            
-        } else if (count == 2) {
-            blue.setAttribute("class", "slide blue inactive");
-            green.setAttribute("class", "slide green active");     
-            return count = 3;
-            
-        } else if (count == 3) {
-            green.setAttribute("class", "slide green inactive");
-            red.setAttribute("class", "slide red active");  
-            return count = 0;
-           
-        }
-        
-
+  if (count == 0) {
+    red.setAttribute("class", "slide red inactive");
+    purple.setAttribute("class", "slide purple active");
+    return (count = 1);
+  } else if (count == 1) {
+    purple.setAttribute("class", "slide purple inactive");
+    blue.setAttribute("class", "slide blue active");
+    return (count = 2);
+  } else if (count == 2) {
+    blue.setAttribute("class", "slide blue inactive");
+    green.setAttribute("class", "slide green active");
+    return (count = 3);
+  } else if (count == 3) {
+    green.setAttribute("class", "slide green inactive");
+    red.setAttribute("class", "slide red active");
+    return (count = 0);
+  }
 }
-
 
 let red = document.querySelector(".red");
 let green = document.querySelector(".green");
@@ -69,6 +44,5 @@ let blue = document.querySelector(".blue");
 let purple = document.querySelector(".purple");
 let count = 0;
 
-
-document.getElementById('next').addEventListener('click',next());
-document.getElementById('next').addEventListener('click',next());
+document.getElementById("next").addEventListener("click", next());
+document.getElementById("previous").addEventListener("click", previous());
